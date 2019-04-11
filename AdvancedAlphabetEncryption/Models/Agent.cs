afb21f1b2d1d9828978f3f4f62625b569265b196
Sean <seanedwards97@gmail.com>
2019-04-11 10:21:03 +0100

    Initial commit

---
+++ AdvancedAlphabetEncryption/Models/Agent.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AdvancedAlphabetEncryption.Models
+{
+    public class Agent
+    {
+        private string _firstName, _lastName, _email;
+        private char[] _initials = new char[2];
+        //private int _ID;
+
+        public static int globalAgentID;
+
+
+
+        public Agent(string _firstName, string _lastName, string _email)
+        {
+            FirstName = _firstName;
+            LastName = _lastName;
+            Email = _email;
+        }
+
+        public string FirstName
+        {
+            get => _firstName;
+
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                    _firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get => _lastName;
+
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                    _lastName = value;
+            }
+        }
+
+        public string Initials
+        {
+            get
+            {
+                try
+                {
+                    _initials[0] = _firstName[0];
+                    _initials[1] = _lastName[0];
+
+                    return new string(_initials);
+                }
+                catch(NullReferenceException)
+                {
+                    return "";
+                }
+
+                
+            }
+        }
+
+        public string Email
+        {
+            get => _email;
+
+            private set
+            {
+                try
+                {
+                    MailAddress mail = new MailAddress(value);
+                    _email = mail.Address;
+                }
+                catch (FormatException) { Console.WriteLine("Invalid Email Format!"); }
+                catch (ArgumentException) { Console.WriteLine("Please check your input and try again."); }
+
+            }
+        }
+
+        public void DisplayInformation()
+        {
+            Console.WriteLine("------------------");
+            Console.WriteLine("Agent Name: {0} {1}", FirstName, LastName);
+            Console.WriteLine("Initials: {0}", Initials);
+            Console.WriteLine("Email: {0}", Email);
+        }
+
+    }
+}
---
+++ AdvancedAlphabetEncryption/Models/PoemReader.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedAlphabetEncryption.Models
+{
+    public class PoemReader
+    {
+        string POEM1 = @"W:\Applied Programming\Assignment\AdvancedAlphabetEncryption\AdvancedAlphabetEncryption\Poems\POEM1.txt";
+        string POEM2 = @"W:\Applied Programming\Assignment\AdvancedAlphabetEncryption\AdvancedAlphabetEncryption\Poems\POEM2.txt";
+        string POEM3 = @"W:\Applied Programming\Assignment\AdvancedAlphabetEncryption\AdvancedAlphabetEncryption\Poems\POEM3.txt";
+
+        public PoemReader (int poemSelection, int lineSelection, int wordSelection)
+        {
+            lineSelection--;
+            wordSelection--;
+
+            string[] lines = new string[0];
+            string[] words;
+            
+            switch (poemSelection)
+            {
+                case 1:
+                    lines = System.IO.File.ReadAllLines(POEM1);
+                    break;
+                case 2:
+                    lines = System.IO.File.ReadAllLines(POEM2);
+                    break;
+                case 3:
+                    lines = System.IO.File.ReadAllLines(POEM3);
+                    break;
+            }
+
+            words = lines[lineSelection].Split();
+            Console.WriteLine(words[wordSelection]);
+
+            
+
+
+        }
+    }
+}
