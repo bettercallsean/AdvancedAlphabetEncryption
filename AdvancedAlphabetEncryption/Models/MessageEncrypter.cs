e386389d0db3498da992035bb4af4c82829f7064
Sean <seanedwards97@gmail.com>
2019-04-19 20:51:20 +0100

    Initial commit

---
+++ AdvancedAlphabetEncryption/Models/MessageEncrypter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedAlphabetEncryption.Models
+{
+    class MessageEncrypter
+    {
+        public MessageEncrypter(string message)
+        {
+
+        }
+    }
+}
