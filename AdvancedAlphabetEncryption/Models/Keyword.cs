d1859c1329662bdecf5e26e48ae7ba22bb67c783
Sean <seanedwards97@gmail.com>
2019-04-25 16:00:49 +0100

    Add checks to ensure that values passed in aren't out of bounds and add a random keyword generator method

--- AdvancedAlphabetEncryption/Models/Keyword.cs
+++ AdvancedAlphabetEncryption/Models/Keyword.cs
@@ -40,22 +40,61 @@ namespace AdvancedAlphabetEncryption.Models
                     break;
             }
 
-            try
+            // Performs checks to ensure that the numbers that have been passed through don't throw an IndexOutOfRange exception
+            // By setting ValidKeyword to false, it will allow a while loop to keep asking for input until these conditions are satisfied
+            if (lineSelection > poem.Count || lineSelection < 1)
             {
-                KeywordCode = string.Format("{0}.{1}.{2}", poemSelection, lineSelection, wordSelection);
-
-                // Accounting for the 0-based array indexing of the words (word 1 will be at index 0)
-                wordSelection--;
-
-                wordArray = poem[lineSelection];
-                KeywordString = wordArray[wordSelection];
-                ValidKeyword = true;
-            }
-            catch (IndexOutOfRangeException)
-            {
-                Console.WriteLine("Index out of bounds!");
                 ValidKeyword = false;
+                return;
             }
+
+            if(wordSelection > poem[lineSelection].Length || wordSelection < 1)
+            {
+                ValidKeyword = false;
+                return;
+            }
+
+            // Adds '0' padding to the number. e.g. '8' will turn to '08' but '14' will stay as '14'
+            // Will produce a code in the following format - XX.XX.XX
+            KeywordCode = string.Format("{0}.{1}.{2}", poemSelection.ToString().PadLeft(2, '0'), lineSelection.ToString().PadLeft(2, '0'), wordSelection.ToString().PadLeft(2, '0'));
+
+            // Accounting for the 0-based array indexing of the words (word 1 will be at index 0)
+            wordSelection--;
+
+            wordArray = poem[lineSelection];
+            KeywordString = wordArray[wordSelection];
+            ValidKeyword = true;
+        }
+
+        public void GenerateRandomKeyword()
+        {
+            Random random = new Random();
+            Dictionary<int, string[]> poem = new Dictionary<int, string[]>();
+            string[] wordArray;
+
+            int poemSelection = random.Next(1, 3);
+
+            switch (poemSelection)
+            {
+                case 1:
+                    poem = JsonConvert.DeserializeObject<Dictionary<int, string[]>>(POEM1);
+                    break;
+                case 2:
+                    poem = JsonConvert.DeserializeObject<Dictionary<int, string[]>>(POEM2);
+                    break;
+                case 3:
+                    poem = JsonConvert.DeserializeObject<Dictionary<int, string[]>>(POEM3);
+                    break;
+            }
+
+            int lineSelection = random.Next(1, poem.Count);
+
+            wordArray = poem[lineSelection];
+            int wordSelection = random.Next(0, wordArray.Length);
+
+            KeywordString = wordArray[wordSelection];
+            ValidKeyword = true;
+
         }
 
         public string KeywordString
@@ -74,7 +113,8 @@ namespace AdvancedAlphabetEncryption.Models
 
         public string KeywordCode { get; private set; }
 
-        private bool ValidKeyword { get; set; } = false;
+        // When a Keyword object is created, there will be no valid keyword assigned to it
+        public bool ValidKeyword { get; set; } = false;
 
     }
 }
