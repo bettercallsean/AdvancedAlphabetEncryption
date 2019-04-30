2b4ecd898b0d5055e1cbf333e89714764758e01e
Sean <seanedwards97@gmail.com>
2019-04-30 19:53:19 +0100

    Create login window

---
+++ AdvancedAlphabetEncryption/ViewModels/LoginViewModel.cs
@@ -0,0 +1,28 @@
+﻿using AdvancedAlphabetEncryption.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedAlphabetEncryption.ViewModels
+{
+    public class LoginViewModel : ViewModelBase
+    {
+        private Agent _agent = new Agent();
+        public string Email
+        {
+            get => _agent.Email;
+
+            set
+            {
+                if(!string.IsNullOrWhiteSpace(value))
+                {
+                    _agent.Email = value;
+
+                    OnPropertyChanged();
+                }
+            }
+        }
+    }
+}
