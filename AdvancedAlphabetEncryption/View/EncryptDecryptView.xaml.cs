6158c6b933e22590143b4f782af6b8dff8f507bc
Sean <seanedwards97@gmail.com>
2019-04-30 13:09:44 +0100

    Move Encrypt/Decrypt layout to seperate view for use as a tab item

--- AdvancedAlphabetEncryption/View/EncryptDecryptView.xaml.cs
+++ AdvancedAlphabetEncryption/View/EncryptDecryptView.xaml.cs
@@ -13,14 +13,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace AdvancedAlphabetEncryption
+namespace AdvancedAlphabetEncryption.View
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for EncryptDecryptView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class EncryptDecryptView : UserControl
     {
-        public MainWindow()
+        public EncryptDecryptView()
         {
             InitializeComponent();
         }
