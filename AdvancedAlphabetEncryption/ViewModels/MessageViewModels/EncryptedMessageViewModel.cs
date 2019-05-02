196dcff7b977730a2415cd30d954dbd37fa2cc77
Sean <seanedwards97@gmail.com>
2019-05-02 14:25:16 +0100

    Move Message ViewModels to subfolder

---
+++ AdvancedAlphabetEncryption/ViewModels/MessageViewModels/EncryptedMessageViewModel.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using AdvancedAlphabetEncryption.AlphabetEncryptionDbContext;
+using AdvancedAlphabetEncryption.Models;
+using AdvancedAlphabetEncryption.Models.Messages;
+using Microsoft.Win32;
+
+namespace AdvancedAlphabetEncryption.ViewModels
+{
+    public class EncryptedMessageViewModel : MessageViewModel
+    {
+        readonly private EncryptedMessage _encryptedMessage = new EncryptedMessage();
+
+        public EncryptedMessageViewModel()
+        {
+            _encryptedMessage.Keyword = App.keyword.KeywordString;
+            _encryptedMessage.CreatedBy = App.agent.Initials;
+        }
+
+        public string MessageString
+        {
+            get => _encryptedMessage.MessageString;
+
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                {
+                    _encryptedMessage.MessageString = value;
+
+                    if (IsEncrypted)
+                        IsEncrypted = false;
+                }
+
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand EncryptCommand { get => new RelayCommand(o => EncryptMessage()); }
+
+        public void EncryptMessage()
+        {
+            if (!IsEncrypted)
+            {
+                _encryptedMessage.Encrypt();
+                IsEncrypted = true;
+
+                OnPropertyChanged("MessageString");
+
+                SaveToDatabase();
+            }
+
+            if (SaveToFileChecked)
+                SaveToFile(_encryptedMessage);
+
+        }
+
+        private void SaveToDatabase()
+        {
+            using (var db = new AdvancedAlphabetEncryptionContext())
+            {
+                db.EncryptedMessages.Add(_encryptedMessage);
+                db.SaveChanges();
+            }
+        }
+
+    }
+
+
+
+}
---
+++ AdvancedAlphabetEncryption/ViewModels/MessageViewModels/MessageViewModel.cs
@@ -0,0 +1,49 @@
+﻿using AdvancedAlphabetEncryption.Models;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AdvancedAlphabetEncryption.ViewModels
+{
+    public class MessageViewModel : BaseViewModel
+    {
+        private bool _isEncrypted;
+        public bool IsEncrypted
+        {
+            get => _isEncrypted;
+            set { _isEncrypted = value; OnPropertyChanged(); }
+        }
+
+        // Used to determine whether the SaveFileDialog box appears
+        private bool _saveToFileChecked = false;
+        public bool SaveToFileChecked
+        {
+            get => _saveToFileChecked;
+            set { _saveToFileChecked = value; OnPropertyChanged(); }
+        }
+
+        public void SaveToFile(Message message)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog
+            {
+                FileName = message.CreationDate.ToString("yyyyMMddHHmmss"),
+                Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*",
+                Title = "Save an encrypted file"
+            };
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                using (System.IO.StreamWriter file = new System.IO.StreamWriter(saveFileDialog.FileName))
+                {
+                    file.WriteLine(message.MessageString);
+                    file.WriteLine(App.keyword.KeywordString);
+                }
+            }
+
+        }
+    }
+}
