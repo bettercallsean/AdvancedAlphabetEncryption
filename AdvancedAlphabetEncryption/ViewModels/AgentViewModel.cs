3304b3b0a161f09cb6abfeb427548e7efa08454d
Sean <seanedwards97@gmail.com>
2019-05-02 14:29:19 +0100

    Create new ViewModels

---
+++ AdvancedAlphabetEncryption/ViewModels/AgentViewModel.cs
@@ -0,0 +1,25 @@
+﻿using AdvancedAlphabetEncryption.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace AdvancedAlphabetEncryption.ViewModels
+{
+    public class AgentViewModel : BaseViewModel
+    {       
+        public string FullName { get => FirstName + " " + LastName; }
+
+        public string FirstName { get => App.agent.FirstName; }
+
+        public string LastName { get => App.agent.LastName; }
+
+        public string Email { get => App.agent.Email; }
+
+        public BitmapImage ProfilePicture { get => Utilities.ImageConverter.LoadImage(App.agent.ProfilePicture); }
+
+    }
+}
---
+++ AdvancedAlphabetEncryption/ViewModels/KeywordViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedAlphabetEncryption.ViewModels
+{
+    public class KeywordViewModel
+    {
+        public string Keyword { get => App.keyword.KeywordString; }
+        public string KeywordDate { get => App.keyword.DaySet.ToString("dddd dd MMM"); }
+    }
+}
