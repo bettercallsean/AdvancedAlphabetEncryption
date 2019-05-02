ec4cc5632f86fec8f0c888c3f69c09501d11a3e2
Sean <seanedwards97@gmail.com>
2019-05-03 00:08:37 +0100

    Create ViewModels for handling MVVM login implementation

---
+++ AdvancedAlphabetEncryption/ViewModels/CloseableViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedAlphabetEncryption.ViewModels
+{
+    // Solution taken from https://stackoverflow.com/a/11948550/7416597
+    public abstract class CloseableViewModel : BaseViewModel
+    {
+        public event EventHandler ClosingRequest;
+
+        protected void OnClosingRequest()
+        {
+            ClosingRequest?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ AdvancedAlphabetEncryption/ViewModels/LoginViewModel.cs
@@ -0,0 +1,58 @@
+﻿using AdvancedAlphabetEncryption.AlphabetEncryptionDbContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace AdvancedAlphabetEncryption.ViewModels
+{
+    public class LoginViewModel : CloseableViewModel
+    {
+
+        private string _email;
+        public string Email
+        {
+            get => _email;
+            set { _email = value; OnPropertyChanged(); }
+        }
+
+        private bool _successfulLogin;
+        public bool SuccessfulLogin
+        {
+            get => _successfulLogin;
+            set { _successfulLogin = value; OnPropertyChanged(); }
+        }
+
+        public ICommand LoginCommand { get => new RelayCommand(o => Login(o)); }
+
+        public void Login(object parameter)
+        {
+            var passwordBox = parameter as PasswordBox;
+            using (var db = new AdvancedAlphabetEncryptionContext())
+            {
+                var query = db.Agent.Where(a => a.Email == Email).FirstOrDefault();
+
+
+                if (query != null && query.ValidPassword(passwordBox.Password))
+                {
+                    SuccessfulLogin = true;
+                    App.agent = query;
+
+                    Application.Current.MainWindow = new MainWindow();
+                    Application.Current.MainWindow.Show();
+
+                    OnClosingRequest();
+
+                }
+                else
+                    SuccessfulLogin = false;
+                
+
+            }
+        }
+    }
+}
