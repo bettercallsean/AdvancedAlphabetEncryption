cb28b20d2ed55dcdee2d39be5d2593e0ce81c93f
Sean <seanedwards97@gmail.com>
2019-05-02 14:28:36 +0100

    Create Utilities folder

---
+++ AdvancedAlphabetEncryption/Utilities/ImageConverter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace AdvancedAlphabetEncryption.Utilities
+{
+    public static class ImageConverter
+    {
+        public static byte[] ImageToByteArray(Image imageIn)
+        {
+            MemoryStream ms = new MemoryStream();
+            imageIn.Save(ms, System.Drawing.Imaging.ImageFormat.Gif);
+            return ms.ToArray();
+        }
+
+        public static Image ByteArrayToImage(byte[] byteArrayIn)
+        {
+            MemoryStream ms = new MemoryStream(byteArrayIn);
+            Image returnImage = Image.FromStream(ms);
+            return returnImage;
+        }
+
+        public static byte[] PathToByte(string imagePath)
+        {
+            Image returnImage = Image.FromFile(imagePath);
+            return ImageToByteArray(returnImage);
+        }
+
+        public static BitmapImage LoadImage(byte[] imageData)
+        {
+            if (imageData == null || imageData.Length == 0) return null;
+            var image = new BitmapImage();
+            using (var ms = new MemoryStream(imageData))
+            {
+                ms.Position = 0;
+                image.BeginInit();
+                image.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
+                image.CacheOption = BitmapCacheOption.OnLoad;
+                image.UriSource = null;
+                image.StreamSource = ms;
+                image.EndInit();
+            }
+            image.Freeze();
+            return image;
+        }
+    }
+}
