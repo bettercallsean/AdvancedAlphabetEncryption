307f5577ef90bd788f1c7ac94896cb749cef99e7
Sean <seanedwards97@gmail.com>
2019-05-06 10:17:57 +0100

    Change ValidKeyword get to a single statement

--- AdvancedAlphabetEncryption/ViewModels/KeywordViewModel.cs
+++ AdvancedAlphabetEncryption/ViewModels/KeywordViewModel.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
@@ -63,13 +64,7 @@ namespace AdvancedAlphabetEncryption.ViewModels
 
         public bool ValidKeyword
         {
-            get
-            {
-                if (ValidPoem && ValidLine && ValidWord)
-                    return true;
-                else
-                    return false;
-            }
+            get => ValidPoem && ValidLine && ValidWord;
         }
         #endregion
 
